60300ab1ef0dcdf569a6b060a075e9fceea1c32b
Hadhemi33 <hadhemi.benmansour@isimg.tn>
2022-11-25 01:00:21 +0100

    first committ

---
+++ app/src/main/java/com/example/blasti/InscriptionClient.java
@@ -0,0 +1,15 @@
+package com.example.blasti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class InscriptionClient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inscription_client);
+    }
+}
---
+++ app/src/main/java/com/example/blasti/signinchoix.java
@@ -0,0 +1,15 @@
+package com.example.blasti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class signinchoix extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signinchoix);
+    }
+}
